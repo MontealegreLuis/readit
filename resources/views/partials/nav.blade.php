df4ba3250a8da0e249f5fc309e19b2c563b500f2
Luis Montealegre <montealegreluis@gmail.com>
2015-11-26 10:18:47 -0600

    Show link to post links only for readitors.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -14,21 +14,11 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="/">Home</a></li>
-                <!--- li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li -->
+                @if (isset($readitor))
+                    <li class="active">
+                        <a href="/links/create">Post a link</a>
+                    </li>
+                @endif
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 @if (!isset($readitor))
@@ -42,7 +32,6 @@
                     </li>
                     <li><a href="/auth/logout">Logout</a></li>
                 @endif
-                <!--- li><a href="../navbar-fixed-top/">Fixed top</a></li --->
             </ul>
         </div>
     </div>
